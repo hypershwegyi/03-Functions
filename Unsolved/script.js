cfa59a244fa2a447505f7c94aeeb1ca839fef2c6
shweyeetheint <shweyeetheint690@gmail.com>
2024-05-25 21:30:25 +0630

    third

---
+++ Unsolved/script.js
@@ -0,0 +1,13 @@
+// TODO: Write Your JavaScript Code Here
+var A = function(cat,dog){
+    if(cat===dog){
+        console.log("They are equal in type and value")
+    }
+    else if(cat==dog){
+        console.log("They are equal in value")
+    }
+    else{
+        console.log("The values are not equal")
+    }
+}
+A(2,"2")
